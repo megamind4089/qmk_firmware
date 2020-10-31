368406f978933895b1f333ab3e3155c50eb6063d
Mega Mind <68985133+megamind4089@users.noreply.github.com>
2020-10-31 13:44:21 +0800

    My Lily58 keymap

---
+++ keyboards/lily58/keymaps/megamind/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define MASTER_LEFT
+
+#define USE_SERIAL_PD2
+
+#define ENCODERS_PAD_A { F4 }
+#define ENCODERS_PAD_B { F5 }
+#define ENCODER_RESOLUTION 4
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 180
+
+#undef RGBLED_NUM
